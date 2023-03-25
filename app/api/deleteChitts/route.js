ec2ca84c8f86585cc28ee92c7ab0d9ca1844c51f
vinay kulkarni <kulkarnivinay621@gmail.com>
2023-03-25 14:04:58 +0530

    updated the api

--- app/api/deleteChitts/route.js
+++ app/api/deleteChitts/route.js
@@ -4,7 +4,7 @@ export async function GET(request) {
   console.log(request.url);
   const client = await connect();
   const db = client.db();
-  // await db.collection(collectionName).deleteMany({});
+  await db.collection(collectionName).deleteMany({});
   client.close();
   return new Response("Hello, Next.js!");
 }
