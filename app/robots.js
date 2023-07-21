efff2cd28013e6265e2145e2d6727550d749693d
vinay kulkarni <kulkarnivinay621@gmail.com>
2023-07-21 19:27:53 +0530

    robots.js

---
+++ app/robots.js
@@ -0,0 +1,17 @@
+export default function robots() {
+  return {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+  }
+}
